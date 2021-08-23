9a76539b3d51ad3a31482f06bf03610304aaacee
vukieuanh <vukieuanh.hnue@gmail.com>
2021-08-23 09:19:17 +0700

    add constance muc1, muc 2, muc 3

--- b1/js.js
+++ b1/js.js
@@ -1,3 +1,7 @@
+const MUC2 = 1.734;
+const MUC1= 1.678;
+const MUC3 = 2.014;
+
 function getMoney(){
     let number1 = document.getElementById("old").value;
     let number2 = document.getElementById("new").value;
@@ -6,16 +10,16 @@ function getMoney(){
     let result = number2 - number1;
     let money=0;
     if (result<=50){
-        money = result*1.678;
+        money = result*MUC1;
         console.log("khoang 1");
     }
     else {
         if (result<=100){
-            money = 50*1.678 + (result - 50)*1.734;
+            money = 50*MUC1 + (result - 50)*MUC2;
             console.log("khoang 2");
         }
         else {
-            money = 50*1.678 + 50*1.743 + (result-100)*2.014;
+            money = 50*MUC1 + 50*MUC2 + (result-100)*MUC3;
             console.log("khoang 3");
         }
     }
